01c305ef35c697b6e48a7ea77130b8f8a0b1658e
samuelmoiwa <samuelmoiwa@gmail.com>
2023-08-03 02:03:05 -0700

    update tailwind clss config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  mode: 'jit',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        'hero-bg': '#F5FCFF',
+        'purple': '#5E3BEE',
+        'purple-header': '#282938',
+        'purple-body': '#1C1E53',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        noto: ['Noto Serif TC', 'serif'],
+      }
+    },
+  },
+  variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    ["@babel/plugin-transform-react-jsx", { "throwIfNamespace": false }]
+  ],
 }
-
