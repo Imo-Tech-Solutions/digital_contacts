4798a9b78cbf6cb1d6a7070d29dab5d3b343dbe0
samuelmoiwa <samuelmoiwa@gmail.com>
2023-08-07 07:33:34 -0700

    update home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -183,9 +183,6 @@ function Home() {
       </div>
     </section>
 
-
-
-
     <section className="bg-white py-16">
       <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-4xl font-semibold text-gray-800 mb-8 text-center">Choose Your Plan</h2>
