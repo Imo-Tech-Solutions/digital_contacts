97aa22831f92f8356e77bc4ef7aee3ff1970a075
samuelmoiwa <samuelmoiwa@gmail.com>
2023-08-25 14:55:17 +0000

    update home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -393,7 +393,6 @@ function Home() {
 
 
 
-
       <section className="bg-white py-16" id="pricing">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-4xl font-semibold text-gray-800 mb-8 text-center">
